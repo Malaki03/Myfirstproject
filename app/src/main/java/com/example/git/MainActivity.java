2565f6fc4d8a9721fd647334f4a250fe226dbbbf
Malaki03 <frihmatmostafa@gmail.com>
2022-12-15 03:03:50 +0100

    this is our second commit

--- app/src/main/java/com/example/git/MainActivity.java
+++ app/src/main/java/com/example/git/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.git;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView txt ;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt = findViewById(R.id.txt) ;
+        txt.setText("this is contained in my seconde commit");
+
     }
 }
